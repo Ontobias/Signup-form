a0ec7bfb20599643e180661a91d98d33d2eb24b6
Oscar T <168926164+Ontobias@users.noreply.github.com>
2025-05-19 10:24:49 +0200

    Add files via upload

---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+$conn = new mysqli($servername, $username, $password, $database) or die(myslq_error());
+
+
+?>
---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width,initial-scale=1">
+    <title>Form Login and Register</title>
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+    <div class="login">
+        <h1>Login</h1>
+        <form>
+            <label>Email</label>
+            <input type="email" name="email" required>
+            <label>Password</label>
+            <input type="password" name="pass" required>
+            <input type="submit" name="" value="Submit">
+        </form>
+        <p>Don't have an account? <a href="Signup.php">Sign up here</a></p>
+</body>
